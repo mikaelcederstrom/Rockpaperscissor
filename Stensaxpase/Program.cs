a03c220ca247c82a596c73f33bc1f8dcdf1c48be
Mikael Cederström <mikael.cederstrom@gmail.com>
2019-02-02 19:46:52 +0100

    Changed some variables names. Made the code shorter and more easy to read. Option to start over.

--- Stensaxpase/Program.cs
+++ Stensaxpase/Program.cs
@@ -1,153 +1,120 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Security.Policy;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Win32.SafeHandles;
 
 namespace Stensaxpase
 {
     class Program
     {
         static int scorePlayer1;
-        static int scoreplayer2;
-        
+        static int scorePlayer2;
+
         static void Main(string[] args)
         {
+            StartingGame();
+            PlayingGame();
+            EndingGame();
+        }
 
-            
-            string player1;
-            Random rnd = new Random();
-            Console.WriteLine("Välj mellan sten, sax och påse genom att skriva den tillhörande siffran");
+        static void StartingGame()
+        {
+            Console.WriteLine("Ett spel av sten, sax, påse. Först till fem som gäller");
+            Console.WriteLine(@"Välj mellan sten, sax och påse genom att skriva den tillhörande siffran");
             Console.WriteLine("1. Sten");
             Console.WriteLine("2. Sax");
             Console.WriteLine("3. Påse");
-            player1 = Console.ReadLine();
-            bool y = true;
+        }
 
+        static void PlayingGame()
+        {
+            Random rnd = new Random();
+            int player1Move = Convert.ToInt32(Console.ReadLine());
 
-            while (true)
+            while (scorePlayer1 < 5 && scorePlayer2 < 5)
             {
+                int player2Move = rnd.Next(1, 4);
+                OutputMove(player1Move, "Du");
+                OutputMove(player2Move, "Datorn");
 
-
-                var x = Convert.ToInt32(player1);
-
-                if (x == 1)
+                if (player1Move == player2Move)
                 {
-
-                    Console.WriteLine("Du valde sten");
-                }
-                else if (x == 2)
-                {
-
-                    Console.WriteLine("Du valde sax");
-
-                }
-                else if (x == 3)
-                {
-                    Console.WriteLine("Du valde påse");
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
-                    Console.WriteLine("Det är ett ogiligt val");
-                    y = false;
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine("Det blev lika");
                     Console.ResetColor();
                 }
 
-                int player2 = rnd.Next(1, 4);
-
-                if (y == true)
+                else if ((player1Move == 1 && player2Move == 2) || (player1Move == 2 && player2Move == 3) ||
+                         (player1Move == 3 && player2Move == 1))
                 {
-
-
-                    if (player2 == 1)
-                    {
-                        Console.WriteLine("Datorn valde sten");
-                    }
-                    else if (player2 == 2)
-                    {
-                        Console.WriteLine("Datorn valde sax");
-                    }
-                    else if (player2 == 3)
-                    {
-                        Console.WriteLine("Datorn valde påse");
-                    }
-
-                    if (x == player2)
-                    {
-                        Console.ForegroundColor = ConsoleColor.DarkYellow;
-                        Console.WriteLine("Det blev lika");
-                        Console.ResetColor();
-                    }
-
-
-                    else if (x == 1 && player2 == 2)
-                    {
-                        Console.ForegroundColor = ConsoleColor.DarkGreen;
-                        Console.WriteLine("Du vann");
-                        scorePlayer1++;
-                        Console.ResetColor();
-
-                    }
-
-                    else if (x == 1 && player2 == 3)
-                    {
-                        Console.ForegroundColor = ConsoleColor.DarkRed;
-                        Console.WriteLine("Du förlorade");
-                        scoreplayer2++;
-                        Console.ResetColor();
-                    }
-
-                    else if (x == 2 && player2 == 3)
-                    {
-                        Console.ForegroundColor = ConsoleColor.DarkGreen;
-                        Console.WriteLine("Du vann");
-                        scorePlayer1++;
-                        Console.ResetColor();
-                    }
-
-                    else if (x == 2 && player2 == 1)
-                    {
-                        Console.ForegroundColor = ConsoleColor.DarkRed;
-                        Console.WriteLine("Du förlorade");
-                        scoreplayer2++;
-                        Console.ResetColor();
-                    }
-
-                    else if (x == 3 && player2 == 1)
-                    {
-                        Console.ForegroundColor = ConsoleColor.DarkGreen;
-                        Console.WriteLine("Du vann");
-                        scorePlayer1++;
-                        Console.ResetColor();
-                    }
-
-                    else if (x == 3 && player2 == 2)
-                    {
-                        Console.ForegroundColor = ConsoleColor.DarkRed;
-                        Console.WriteLine("Du förlorade");
-                        scoreplayer2++;
-                        Console.ResetColor();
-                    }
-
-                    Console.WriteLine($"Du har {scorePlayer1} poäng & datorn har {scoreplayer2} poäng");
-                    Console.WriteLine("Välj ett nytt nummer för att spela igen");
-                    player1 = Console.ReadLine();
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine("Du vann");
+                    scorePlayer1++;
+                    Console.ResetColor();
                 }
-                else
+
+                else if ((player1Move == 1 && player2Move == 3) || (player1Move == 2 && player2Move == 1) ||
+                         (player1Move == 3 && player2Move == 2))
                 {
-                    Console.WriteLine("Testa att skriva ett giltigt nummer");
-                    player1 = Console.ReadLine();
-                    y = true;
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("Du förlorade");
+                    scorePlayer2++;
+                    Console.ResetColor();
+                }
+
+                Console.WriteLine($"Du har {scorePlayer1} poäng & datorn har {scorePlayer2} poäng");
+                Console.WriteLine();
+                if (scorePlayer1 < 5 && scorePlayer2 < 5)
+                {
+                    Console.WriteLine("Välj ett nummer på nytt");
+                    player1Move = Convert.ToInt32(Console.ReadLine());
                 }
 
             }
+        }
 
-            Console.ReadKey();
+        static void OutputMove(int move, string name)
+        {
+            if (move == 1)
+            {
+                Console.WriteLine($"{name} valde sten");
+            }
+            else if (move == 2)
+            {
+                Console.WriteLine($"{name} valde sax");
+            }
+            else if (move == 3)
+            {
+                Console.WriteLine($"{name} valde påse");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Det är ett ogiligt val");
+                Console.ResetColor();
+            }
 
         }
+
+        static void EndingGame()
+        {
+            if (scorePlayer1 > scorePlayer2)
+            {
+                Console.WriteLine($"Du vinner med {scorePlayer1} poäng emot datorns {scorePlayer2} poäng");
+            }
+            else
+            {
+                Console.WriteLine($"Datorn vinner med {scorePlayer2} poäng emot dina {scorePlayer1} poäng");
+            }
+
+            Console.WriteLine("Om du vill spela igen trycker du y");
+            string playAgain = Console.ReadLine();
+            if (playAgain == "y")
+            {
+                Console.WriteLine();
+                scorePlayer1 = 0;
+                scorePlayer2 = 0;
+                Main(null);
+            }
+
+        }
+
     }
 }
